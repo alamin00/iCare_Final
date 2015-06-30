701524b9faaa9055d4f48cf1cc86561dc33244ea
alamin00 <alamintvn@gmail.com>
2015-06-30 17:58:07 +0600

    contact with doctor

--- app/src/main/java/com/crackbrain/tanveen/icare/profileimagedemo/Fragment/OrderListFragment.java
+++ app/src/main/java/com/crackbrain/tanveen/icare/profileimagedemo/Fragment/OrderListFragment.java
@@ -2,22 +2,28 @@ package com.crackbrain.tanveen.icare.profileimagedemo.Fragment;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.crackbrain.tanveen.icare.R;
 import com.crackbrain.tanveen.icare.profileimagedemo.DoctorActivity;
 import com.crackbrain.tanveen.icare.profileimagedemo.Helpers.Constants;
 import com.crackbrain.tanveen.icare.profileimagedemo.*;
+import com.crackbrain.tanveen.icare.profileimagedemo.Model.DoctorCal;
 
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
  */
 public class OrderListFragment extends Fragment {
+
+
+
 
 
     public OrderListFragment() {
@@ -37,13 +43,30 @@ public class OrderListFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
 
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_list, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_order_list, container, false);
+
+        final   Button btnCall = (Button)view.findViewById(R.id.btnDoctorCall);
+
+       btnCall.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent in = new Intent(getActivity(),DoctorCal.class);
+               startActivity(in);
+           }
+       });
+
+
+        return view;
     }
 
     @Override
